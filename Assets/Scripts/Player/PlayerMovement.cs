6d97115c7fb430f5063c750ae5f5bc4ffbf04c72
URANO000 <kkonigin1100@gmail.com>
2026-07-03 00:19:29 -0600

    Everything actually looks good now in terms of movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
     [Header("Movement")]
     public float moveSpeed = 5f;
 
@@ -20,14 +21,46 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 groundCheckSize = new Vector2(0.49f, 0.03f);
     public LayerMask groundLayer;
 
+    [Header("Gravity")]
+    public float baseGravity = 2f;
+    public float maxFallSpeed = 18f;
+    public float fallGravityMult = 2f;
+
+    bool isFacingRight = true;
+    bool isGrounded;
+
+
     void Start()
     {
-        
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
     }
 
     void Update()
     {
-        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+        GroundCheck();
+        Gravity();
+        Flip();
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
+        animator.SetFloat("speed", Mathf.Abs(rb.velocity.x));
+    }
+
+    public void Gravity()
+    {
+        //falling gravity
+        if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = baseGravity * fallGravityMult; //fall faster and faster
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Max(rb.velocity.y, -maxFallSpeed)); //max fall speed
+        }
+        else
+        {
+            rb.gravityScale = baseGravity;
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -44,21 +77,42 @@ public class PlayerMovement : MonoBehaviour
                 //Hold down jump button = full height
                 rb.velocity = new Vector2(rb.velocity.x, jumpPower);
                 jumpsRemaining--;
+                animator.SetTrigger("jump");
             }
             else if (context.canceled && rb.velocity.y > 0)
             {
                 //Light tap of jump button = half the height
                 rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-                jumpsRemaining--;
             }
         }
     }
 
     private void GroundCheck()
     {
-        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) //checks if set box overlaps with ground
+        bool wasGrounded = isGrounded;
+
+        isGrounded = Physics2D.OverlapBox(
+            groundCheckPos.position,
+            groundCheckSize,
+            0,
+            groundLayer);
+
+        if (!wasGrounded && isGrounded)
         {
             jumpsRemaining = maxJumps;
+        }
+
+        animator.SetBool("grounded", isGrounded);
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
         }
     }
 
