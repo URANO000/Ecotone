0e028d6edcea1ce106e70ffc3b9aba95375eb444
URANO000 <kkonigin1100@gmail.com>
2026-08-08 22:52:45 -0600

    Porcupine
    Moves

---
+++ Assets/Scripts/Interactables/FlipWalk.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipWalk : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float moveSpeed = 4f;
+
+    private Vector3 nextPosition;
+    private bool isMovingToB = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPosition = pointB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+        if (transform.position == nextPosition)
+        {
+            if (isMovingToB)
+            {
+                nextPosition = pointA.position;
+                isMovingToB = false;
+
+                FlipSprite(1);
+            }
+            else
+            {
+                nextPosition = pointB.position;
+                isMovingToB = true;
+
+                FlipSprite(-1);
+            }
+        }
+
+    }
+
+    void FlipSprite(int direction)
+    {
+        Vector3 newScale = transform.localScale;
+        newScale.x = direction * Mathf.Abs(newScale.x);
+        transform.localScale = newScale;
+    }
+}
---
+++ Assets/Scripts/Interactables/PorcupineDamage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PorcupineDamage : MonoBehaviour
+{
+    [SerializeField] private int damageAmount = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount);
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
