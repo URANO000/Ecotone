6b0c3f39bac6cfae25b7955299dbd9d9d3ed8e49
URANO000 <kkonigin1100@gmail.com>
2026-08-11 14:28:28 -0600

    Player dies once they hit water.

---
+++ Assets/Scripts/Interactables/WaterDamage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDamage : MonoBehaviour
+{
+    private int damageAmount = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if(playerHealth != null)
+            {
+                Debug.Log("Player entered water.");
+                playerHealth.TakeDamage(damageAmount);
+            }
+        }
+    }
+}
