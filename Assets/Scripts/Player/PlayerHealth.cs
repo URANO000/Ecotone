ed8d59c58119c00c1532e0d6ffd4f0e106ea1773
URANO000 <kkonigin1100@gmail.com>
2026-08-11 23:56:44 -0600

    Update PlayerHealth.cs

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 100;
+    private int maxHealth = 100;
     [SerializeField] private HealthBar healthBar;
     private Rigidbody2D rb;
     private PlayerMovement playerMovement;
